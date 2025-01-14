50875cdddf7b64de3fd5165628a1757058ed4674
Aryan <gokugonzalez840@gmail.com>
2025-01-14 16:00:47 -0500

    Terminando De Añadir clases

--- codigo.py
+++ codigo.py
@@ -1,41 +1,51 @@
-class Persona:
-    def __init__(self):
-        self.nombre=input("Diganos su nombre para inscribirlo")
-        self.apellido=input("Cual es su apellido")
-        self.concurso=input("Diganos el numero de lista del concurso en que desea inscribirse")
-        self.escuela=input("De que escuela es usted")
-    def __Verificar(self):
-            Escuelas_Disponibles=["Josue Pais Garcia","Rafael Orejon Forment","Frank Pais Garcia"]
-            Concursos_Disponibles={
-                1:"Español Literatura",
-                2:"Fisica",
-                3:"Geografia",
-                4:"Quimica",
-                5:"Matematica",
-                6:"Historia"
-            }
-            
-            while (self.escuela.lower() == Escuelas_Disponibles[0].lower()) | (self.escuela.lower() == Escuelas_Disponibles[1].lower()) | (self.escuela.lower() == Escuelas_Disponibles[2].lower()):
-                print("Tenemos bacantes para escuela")
-                if (self.concurso.lower() != Concursos_Disponibles[1].lower()) | (self.concurso.lower() != Concursos_Disponibles[2].lower()) | (self.concurso.lower() != Concursos_Disponibles[3].lower()) | (self.concurso.lower() != Concursos_Disponibles[4].lower())  | (self.concurso.lower() != Concursos_Disponibles[5].lower()) | (self.concurso.lower() != Concursos_Disponibles[6].lower()):
-                    concurso_validar="Correcto"
-                    return concurso_validar
-                else:
-                    concurso_validar="Incorrecto"
-                    return concurso_validar
-            while  (self.escuela != Escuelas_Disponibles[0]) | (self.escuela != Escuelas_Disponibles[1]) | (self.escuela != Escuelas_Disponibles[2]):
-                 print("No tenemos vancantes para esa escuela ")
-                 break
-    def  _Validar(self,conco):
-        concurso=self.__Verificar()
+import sqlite3
+import pandas as pd
+
+from dataclasses import dataclass
+from typing import Optional,Dict,List,Union
+
+@dataclass   
+class Datos:   
+            Escuelas_Disponibles:List[str]=["Josue Pais Garcia","Rafael Orejon Forment","Frank Pais Garcia"]
+            Concursos_Disponibles:Dict[str|int]={
+                1:"Español Literatura".lower(),
+                2:"Fisica".lower(),
+                3:"Geografia".lower(),
+                4:"Quimica".lower(),
+                5:"Matematica".lower(),
+                6:"Historia".lower()
+          }
+    
+@dataclass           
+class Persona(Datos):     
+      nombre:str=input("Diganos su nombre para inscribirlo: ")
+      apellido:str=input("Cual es su apellido: ")
+      concurso:int|str=input("Diganos el numero de lista del concurso en que desea inscribirse: ")
+      escuela:str=input("De que escuela es usted: ")
+      @classmethod
+      def  _Registrarse(self,concur:Optional[int])->None:    
+       if self.escuela.lower() == Datos().Escuelas_Disponibles[0].lower() | self.escuela.lower() == Datos().Escuelas_Disponibles[1].lower()| self.escuela.lower() == Datos().Escuelas_Disponibles[2].lower():
+           
+        while self.concurso.lower() != Datos().Concursos_Disponibles[1] | self.concurso.lower() != Datos().Concursos_Disponibles[2] | self.concurso.lower() != Datos().Concursos_Disponibles[3] | self.concurso.lower() != Datos().Concursos_Disponibles[4]| self.concurso.lower() != Datos().Concursos_Disponibles[2] | self.concurso.lower() != Datos().Concursos_Disponibles[2]:
+            print("No ahi ningun concurso con ese numero de lista")
+            break
+        while self.concurso.lower() == Datos().Concursos_Disponibles[1] | self.concurso.lower() == Datos().Concursos_Disponibles[2] | self.concurso.lower() == Datos().Concursos_Disponibles[3] | self.concurso.lower() == Datos().Concursos_Disponibles[4]| self.concurso.lower() == Datos().Concursos_Disponibles[2] | self.concurso.lower() == Datos().Concursos_Disponibles[2]:
+            print("No te hemos inscrito en la lista")
+            break
+       else:
+           print("No tenemos vacantes para esa escuela")
+      @property     
+      def GuardarDatos()->None:
+          conectar=sqlite3.connect("base_de_datos.db")
+          
+          GurdarDatos='''
+
+                 '''
+               
+           
         
-        while concurso == "Correcto":
-            print(f"Te has inscrito en el concurso de {self.concurso}")
-            while self.concurso == 1:
-                return
-        while concurso != "Correcto":
-            print(f"Lo sentimos no ahi ningun concurso con ese numero")
-    def GuardarDatos(self):
-        print()
         
-                    
+        
+
+Aryan=Persona()                 
+Aryan._Registrarse()       
